bd49ba8efb2d1c97b40bf3fd3ec46427ab965eb8
vipin018 <vipinpathak018@gmail.com>
2025-06-11 01:38:40 +0530

    created camera and also added resize and update

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -1,11 +1,50 @@
-export default class Camera{
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+import Experience from "./Experience.js";
 
-    constructor(){
+export default class Camera {
 
-        this.experience = window.experience;
+    constructor() {
+
+        this.experience = new Experience();
         this.sizes = this.experience.sizes;
         this.scene = this.experience.scene;
         this.canvas = this.experience.canvas;
+
+        // set the camera instance
+        this.setInstance();
+
+        // set orbit controls
+        this.setOrbitControls();
+
+     
+       
     }
-    
+
+    setInstance() {
+        this.instance = new THREE.PerspectiveCamera(
+            35,
+            this.sizes.width / this.sizes.height,
+            0.1,
+            100
+        )
+        this.instance.position.set(6, 4, 8);
+        this.scene.add(this.instance);
+    }
+
+    setOrbitControls() {
+        this.controls = new OrbitControls(
+            this.instance, this.canvas);
+        this.controls.enableDamping = true;
+    }
+
+    resize() {
+        this.instance.aspect = this.sizes.width / this.sizes.height;
+        this.instance.updateProjectionMatrix();
+    }
+
+    update(){
+        this.controls.update();
+    }
+
 }
--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -1,11 +1,20 @@
 import * as THREE from 'three';
 import Sizes from "./Utils/Sizes.js";
 import Time from './Utils/Time.js'
-// import Camera from './Camera.js';
+import Camera from './Camera.js';
 
+
+let instance = null;
 
 export default class Experience {
     constructor() {
+
+        // if there is already an instance, return it
+        if (instance) {
+            return instance;
+        }
+        // set the instance to this
+        instance = this;
 
         // global access to the experience instance
         window.experience = this;
@@ -16,6 +25,8 @@ export default class Experience {
         // setup
         this.sizes = new Sizes();
         this.time = new Time();
+        this.scene = new THREE.Scene();
+        this.camera = new Camera();
 
         //sizes resize event
         this.sizes.on("resize", () => {
@@ -28,10 +39,10 @@ export default class Experience {
 
     }
     resize() {
-
+this.camera.resize();
     }
 
     update() {
-
+this.camera.update();
     }
 }
